850feea4262727149933921f0bcd4c1ff48fa729
Kovacs Viktor <viktor.kovacs@smartx.eu>
2016-12-22 15:25:35 +0100

    findmissingletter

---
+++ sixkyu/findmissingletter/src/main/java/hu/crs/Kata.java
@@ -0,0 +1,16 @@
+package hu.crs;
+
+public class Kata {
+
+    public static char findMissingLetter(char[] array) {
+        int previous = array[0];
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            if (current - previous > 1) {
+                return ((char) (current - 1));
+            }
+            previous = current;
+        }
+        throw new RuntimeException("Invalid input");
+    }
+}
