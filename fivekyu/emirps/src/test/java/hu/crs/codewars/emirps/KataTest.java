28dc30ada1dd704b27796f5829218faeaf66294e
Viktor Kovacs <crusader8@gmail.com>
2017-01-05 20:53:23 +0100

    use prime generator

--- fivekyu/emirps/src/test/java/hu/crs/codewars/emirps/KataTest.java
+++ fivekyu/emirps/src/test/java/hu/crs/codewars/emirps/KataTest.java
@@ -10,6 +10,8 @@ public class KataTest {
         Assert.assertArrayEquals(new long [] {4, 37, 98}, Kata.findEmirp(50));
         Assert.assertArrayEquals(new long [] {8, 97, 418}, Kata.findEmirp(100));
         Assert.assertArrayEquals(new long [] {446, 14957, 3661772}, Kata.findEmirp(14958));
+        Assert.assertArrayEquals(new long [] {2205, 119993, 137429553}, Kata.findEmirp(119994));
+        Assert.assertArrayEquals(new long [] {2330, 124777, 152727598}, Kata.findEmirp(124778));
     }
 
 
