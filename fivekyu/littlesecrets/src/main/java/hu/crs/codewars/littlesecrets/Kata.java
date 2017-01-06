63f69652f01e49688a689cabcaca896b67328aed
Viktor Kovacs <crusader8@gmail.com>
2017-01-06 12:12:51 +0100

    littlesecrets solved

--- fivekyu/littlesecrets/src/main/java/hu/crs/codewars/littlesecrets/Kata.java
+++ fivekyu/littlesecrets/src/main/java/hu/crs/codewars/littlesecrets/Kata.java
@@ -1,8 +1,20 @@
 package hu.crs.codewars.littlesecrets;
 
+import java.lang.reflect.Field;
+
 public class Kata {
 
     public static void main(String[] args) {
-    }
+        try {
+            final Field mySecret = LittleClass.class.getDeclaredField("secret");
+            mySecret.setAccessible(true);
 
+            String secret = (String) mySecret.get(null);
+            System.out.println(secret);
+            System.out.println(LittleClass.isMySecret(secret));
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
