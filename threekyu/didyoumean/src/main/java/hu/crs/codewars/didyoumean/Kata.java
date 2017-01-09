ae7a3bbb2b314c10abe61f3d34e6a8005e595b5a
Viktor Kovacs <crusader8@gmail.com>
2017-01-09 16:30:47 +0100

    init didyoumean

---
+++ threekyu/didyoumean/src/main/java/hu/crs/codewars/didyoumean/Kata.java
@@ -0,0 +1,15 @@
+package hu.crs.codewars.didyoumean;
+
+public class Kata {
+    private final String[] words;
+
+    public Kata(String[] words) {
+        this.words = words;
+    }
+
+    public String findMostSimilar(String to) {
+        // TODO: this is your task ;)
+        return null;
+    }
+
+}
