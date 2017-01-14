cb5f283f60ce307643d672d12f5cc3ce24ac4492
Viktor Kovacs <crusader8@gmail.com>
2017-01-14 10:06:59 +0100

    SumSquaredDivisors

---
+++ sixkyu/sumsquareddivisors/src/test/java/hu/crs/codewars/sumsquareddivisors/KataTest.java
@@ -0,0 +1,20 @@
+package hu.crs.codewars.sumsquareddivisors;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KataTest {
+    @Test
+    public void test1() {
+        assertEquals("[[1, 1], [42, 2500], [246, 84100]]", Kata.listSquared(1, 250));
+    }
+    @Test
+    public void test2() {
+        assertEquals("[[42, 2500], [246, 84100]]", Kata.listSquared(42, 250));
+    }
+    @Test
+    public void test3() {
+        assertEquals("[[287, 84100]]", Kata.listSquared(250, 500));
+    }
+}
