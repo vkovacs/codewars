78491bba8386e6f702220c329077dd4a0ee16a09
Viktor Kovacs <crusader8@gmail.com>
2017-02-20 14:34:38 +0100

    Reverse polish notation calculator

---
+++ fourkyu/reversepolishnotationcalculator/src/main/java/hu/crs/codewars/reversepolishnotationcalculator/Kata.java
@@ -0,0 +1,47 @@
+package hu.crs.codewars.reversepolishnotationcalculator;
+
+import java.util.Stack;
+
+public class Kata {
+    public double evaluate(String expr) {
+        if ("".equals(expr)) {
+            return 0;
+        }
+        return calculate(parse(expr));
+    }
+
+    private String[] parse(String expr) {
+        return expr.split(" ");
+    }
+
+    private Double calculate(String[] parsedExpr) {
+        Stack<Double> stack = new Stack<Double>();
+        for (String element : parsedExpr) {
+            final char c = element.charAt(0);
+            if (Character.isDigit(c)) {
+                stack.push(Double.parseDouble(element));
+            } else {
+                Double operand1 = stack.pop();
+                Double operand0 = stack.pop();
+                final Double result = operate(operand0, operand1, c);
+                stack.push(result);
+            }
+        }
+        return stack.pop();
+    }
+
+    private Double operate(Double operand0, Double operand1, char operator) {
+        switch (operator) {
+            case '+':
+                return operand0 + operand1;
+            case '-':
+                return operand0 - operand1;
+            case '*':
+                return operand0 * operand1;
+            case '/':
+                return operand0 / operand1;
+            default:
+                throw new IllegalArgumentException("Invalid character in input");
+        }
+    }
+}
