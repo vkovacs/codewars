817966407d26e05d1c3b217f00819c330e3d6ef5
viktor.kovacs <viktor.kovacs@evosoft.com>
2017-10-06 09:34:03 +0200

    move classes

--- onekyu/tinythreepasscompiler/src/test/java/hu/crs/codewars/KataTest.java
+++ onekyu/tinythreepasscompiler/src/test/java/hu/crs/codewars/KataTest.java
@@ -1,5 +1,6 @@
 package hu.crs.codewars;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -18,16 +19,16 @@ public class KataTest {
         // {'op':'/','a':{'op':'-','a':{'op':'+','a':{'op':'*','a':{'op':'*','a':{'op':'imm','n':2},'b':{'op':'imm','n':3}},'b':{'op':'arg','n':0}},'b':{'op':'*','a':{'op':'imm','n':5},'b':{'op':'arg','n':1}}},'b':{'op':'*','a':{'op':'imm','n':3},'b':{'op':'arg','n':2}}},'b':{'op':'+','a':{'op':'+','a':{'op':'imm','n':1},'b':{'op':'imm','n':3}},'b':{'op':'*','a':{'op':'imm','n':2},'b':{'op':'imm','n':2}}}}
         Ast t1 = new BinOp("/", new BinOp("-", new BinOp("+", new BinOp("*", new BinOp("*", new UnOp("imm", 2), new UnOp("imm", 3)), new UnOp("arg", 0)), new BinOp("*", new UnOp("imm", 5), new UnOp("arg", 1))), new BinOp("*", new UnOp("imm", 3), new UnOp("arg", 2))), new BinOp("+", new BinOp("+", new UnOp("imm", 1), new UnOp("imm", 3)), new BinOp("*", new UnOp("imm", 2), new UnOp("imm", 2))));
         Ast p1 = compiler.pass1(prog);
-        assertEquals("Pass 1", t1, p1);
+        Assert.assertEquals("Pass 1", t1, p1);
 
         // {'op':'/','a':{'op':'-','a':{'op':'+','a':{'op':'*','a':{'op':'imm','n':6},'b':{'op':'arg','n':0}},'b':{'op':'*','a':{'op':'imm','n':5},'b':{'op':'arg','n':1}}},'b':{'op':'*','a':{'op':'imm','n':3},'b':{'op':'arg','n':2}}},'b':{'op':'imm','n':8}}
         Ast t2 = new BinOp("/", new BinOp("-", new BinOp("+", new BinOp("*", new UnOp("imm", 6), new UnOp("arg", 0)), new BinOp("*", new UnOp("imm", 5), new UnOp("arg", 1))), new BinOp("*", new UnOp("imm", 3), new UnOp("arg", 2))), new UnOp("imm", 8));
         Ast p2 = compiler.pass2(p1);
-        assertEquals("Pass 2", t2, p2);
+        Assert.assertEquals("Pass 2", t2, p2);
 
         List<String> p3 = compiler.pass3(p2);
-        assertEquals("prog(4,0,0) == 3", 3, Simulator.simulate(p3, 4, 0, 0));
-        assertEquals("prog(4,8,0) == 8", 8, Simulator.simulate(p3, 4, 8, 0));
-        assertEquals("prog(4,8,16) == 2", 2, Simulator.simulate(p3, 4, 8, 16));
+        Assert.assertEquals("prog(4,0,0) == 3", 3, Simulator.simulate(p3, 4, 0, 0));
+        Assert.assertEquals("prog(4,8,0) == 8", 8, Simulator.simulate(p3, 4, 8, 0));
+        Assert.assertEquals("prog(4,8,16) == 2", 2, Simulator.simulate(p3, 4, 8, 16));
     }
 }
