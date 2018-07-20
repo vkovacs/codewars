2ab45588a585ca2c7f64da05ce4d6e2e16a9be3f
Viktor Kovacs <crusader8@gmail.com>
2018-07-20 10:16:56 +0200

    improve solution's speed

--- src/main/java/hu/crs/codewars/fivekyu/powersumdig/Kata.java
+++ src/main/java/hu/crs/codewars/fivekyu/powersumdig/Kata.java
@@ -2,7 +2,9 @@ package hu.crs.codewars.fivekyu.powersumdig;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Numbers that are a power of their sum of digits
@@ -12,11 +14,16 @@ import java.util.List;
 public class Kata {
     private static final int MAX_POWER = 50;
     private static final long MAX_NUMBER_TO_CHECK = 500;
+	private static final Map<Integer, Long> resultMap = new HashMap<>();
 
     public static long powerSumDigTerm(int n) {
         if (n < 1) {
             throw new IllegalArgumentException();
         }
+
+		if (resultMap.get(n) != null) {
+			return resultMap.get(n);
+		}
 
         List<Long> resultList = new ArrayList<>();
 
@@ -30,7 +37,11 @@ public class Kata {
         }
         Collections.sort(resultList);
 
-        return resultList.get(n - 1);
+		for (int i = 0; i < resultList.size(); i++) {
+			resultMap.put(i + 1, resultList.get(i));
+		}
+
+        return resultMap.get(n);
     }
 
     private static long sumOfDigits(long number) {
--- src/test/java/hu/crs/codewars/fivekyu/powersumdig/KataTest.java
+++ src/test/java/hu/crs/codewars/fivekyu/powersumdig/KataTest.java
@@ -1,18 +1,29 @@
 package hu.crs.codewars.fivekyu.powersumdig;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.IntStream;
+import org.junit.Test;
+
 public class KataTest {
-    private static void testing(long act, long exp) {
-        assertEquals(exp, act);
-    }
-    @Test
-    public void test1() {
-        testing(Kata.powerSumDigTerm(1), 81);
-        testing(Kata.powerSumDigTerm(2), 512);
-        testing(Kata.powerSumDigTerm(3), 2401);
-        testing(Kata.powerSumDigTerm(4), 4913);
-    }
+	private static void testing(long act, long exp) {
+		assertEquals(exp, act);
+	}
+
+	@Test
+	public void test1() {
+		IntStream.range(0, 10)
+				.forEach( i -> {
+					LocalDateTime start = LocalDateTime.now();
+		testing(Kata.powerSumDigTerm(1), 81);
+		testing(Kata.powerSumDigTerm(2), 512);
+		testing(Kata.powerSumDigTerm(3), 2401);
+		testing(Kata.powerSumDigTerm(4), 4913);
+		LocalDateTime end = LocalDateTime.now();
+		System.out.println(start.until(end, ChronoUnit.MILLIS));
+		});
+
+	}
 }
