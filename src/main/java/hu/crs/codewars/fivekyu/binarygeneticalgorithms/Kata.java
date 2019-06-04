d6ccf335718b65c74b593c7854a0dd73bfbdbe32
vkovacs <crusader8@gmail.com>
2019-06-04 13:36:45 +0200

    generate is tested

--- src/main/java/hu/crs/codewars/fivekyu/binarygeneticalgorithms/Kata.java
+++ src/main/java/hu/crs/codewars/fivekyu/binarygeneticalgorithms/Kata.java
@@ -1,23 +1,28 @@
 package hu.crs.codewars.fivekyu.binarygeneticalgorithms;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Kata {
 
-    private String generate(int length) {
+    String generate(int length) {
+        return Stream.generate(() -> Integer.toString(ThreadLocalRandom.current().nextInt(2)))
+                .limit(length)
+                .collect(Collectors.joining());
+    }
+
+    String[] select(List<String> population, List<Double> fitnesses) {
         throw new UnsupportedOperationException();
     }
 
-    private String[] select(List<String> population, List<Double> fitnesses) {
+    String mutate(String chromosome, double p) {
         throw new UnsupportedOperationException();
     }
 
-    private String mutate(String chromosome, double p) {
-        throw new UnsupportedOperationException();
-    }
-
-    private String[] crossover(String chromosome1, String chromosome2) {
+    String[] crossover(String chromosome1, String chromosome2) {
         throw new UnsupportedOperationException();
     }
 
--- src/test/java/hu/crs/codewars/fivekyu/binarygeneticalgorithms/KataTest.java
+++ src/test/java/hu/crs/codewars/fivekyu/binarygeneticalgorithms/KataTest.java
@@ -1,5 +1,43 @@
 package hu.crs.codewars.fivekyu.binarygeneticalgorithms;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 public class KataTest {
 
+    private Kata kata;
+
+    @Before
+    public void setUp() throws Exception {
+        kata = new Kata();
+    }
+
+    @Test
+    public void generate() {
+        final int length = 10;
+        String generated = kata.generate(length);
+        assertThat(generated, notNullValue());
+        assertThat(generated.length(), is(length));
+
+        int count0s = (int) generated.chars().filter(ch -> ch == '0').count();
+        int count1s = (int) generated.chars().filter(ch -> ch == '1').count();
+        assertThat(count0s + count1s, is(length));
+    }
+
+    @Test
+    public void generateIsRandom() {
+        final int length = 100_000;
+
+        String a = kata.generate(length);
+        String b = kata.generate(length);
+
+        assertThat(a, notNullValue());
+        assertThat(b, notNullValue());
+        assertThat(a, is(not(b)));
+    }
 }
