e173e0757041dd9cc9798412f8abb52665398064
Viktor Kovacs <crusader8@gmail.com>
2019-10-10 22:34:00 +0200

    optimization

--- src/main/java/hu/crs/codewars/threekyu/findntgdigit/Kata.java
+++ src/main/java/hu/crs/codewars/threekyu/findntgdigit/Kata.java
@@ -6,10 +6,42 @@ package hu.crs.codewars.threekyu.findntgdigit;
  * https://www.codewars.com/kata/59f6e1af3640ce12510000ad/train/java
  */
 public class Kata {
+    private static String firstLine;
+    private static String secondLine;
+
+    private static void generateLines(int minPosition) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        int number1 = 1;
+        int number2 = number1 * number1;
+
+        while (sb1.length() <= minPosition + 1 || sb2.length() <= minPosition + 1) {
+            sb1.append(number1);
+            sb2.append(number2);
+            number1++;
+            number2 = number1 * number1;
+        }
+
+        int i = minPosition;
+        while (Character.getNumericValue(sb1.charAt(i + 1)) + Character.getNumericValue(sb2.charAt(i + 1)) > 8) {
+            sb1.append(number1);
+            sb2.append(number2);
+            number1++;
+            number2 = number1 * number1;
+            i++;
+        }
+        firstLine = sb1.toString();
+        secondLine = sb2.toString();
+    }
+
+
     public static int findDigit(int n) {
         if (n < 0) {
             throw new IllegalArgumentException();
         }
+        generateLines(n);
+
         int currentPrecision = n;
 
         while (nthDigitOfFirstLine(currentPrecision + 1) + nthDigitOfSecondLine(currentPrecision + 1) > 8) {
@@ -32,38 +64,11 @@ public class Kata {
         return Character.getNumericValue(sum.charAt(n));
     }
 
-    public static int nthDigitOfFirstLine(final int n) {
-        int currentNumber = 1;
-        int currentIndex = 0;
-
-        while (currentIndex <= n) {
-            String currentNumberString = String.valueOf(currentNumber);
-            if (currentIndex + currentNumberString.length() <= n) {
-                currentIndex += currentNumberString.length();
-            } else {
-                return Character.getNumericValue(currentNumberString.charAt(n - currentIndex));
-            }
-            currentNumber++;
-        }
-        throw new IllegalStateException();
+    private static int nthDigitOfSecondLine(int i) {
+        return Character.getNumericValue(secondLine.charAt(i));
     }
 
-    public static int nthDigitOfSecondLine(final int n) {
-        int currentNumberOfSecondLine = 1;
-        int currentIndexInSecondLine = 0;
-
-        int i = 1;
-        while (currentIndexInSecondLine <= n) {
-            String currentNumberString = String.valueOf(currentNumberOfSecondLine);
-            if (currentIndexInSecondLine + currentNumberString.length() <= n) {
-                currentIndexInSecondLine += currentNumberString.length();
-            } else {
-                return Character.getNumericValue(currentNumberString.charAt(n - currentIndexInSecondLine));
-            }
-
-            i++;
-            currentNumberOfSecondLine = i * i;
-        }
-        throw new IllegalStateException();
+    private static int nthDigitOfFirstLine(int i) {
+        return Character.getNumericValue(firstLine.charAt(i));
     }
 }
--- src/test/java/hu/crs/codewars/threekyu/findntgdigit/KataTest.java
+++ src/test/java/hu/crs/codewars/threekyu/findntgdigit/KataTest.java
@@ -4,9 +4,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static hu.crs.codewars.threekyu.findntgdigit.Kata.findDigit;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class KataTest {
     @Test
@@ -24,86 +22,5 @@ public class KataTest {
         assertEquals(3, findDigit(10000000));
         assertEquals(9, findDigit(100000000));
         assertEquals(1, findDigit(1000000000));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void finDigitLengthMustBeGreaterOrEqualsThanZero() {
-        findDigit(-1);
-    }
-
-    @Test
-    public void find0thDigitInFirstLine() {
-        assertThat(Kata.nthDigitOfFirstLine(0), is(1));
-    }
-
-    @Test
-    public void find9thDigitInFirstLine() {
-        assertThat(Kata.nthDigitOfFirstLine(9), is(1));
-    }
-
-    @Test
-    public void find10thDigitInFirstLine() {
-        assertThat(Kata.nthDigitOfFirstLine(10), is(0));
-    }
-
-    @Test
-    public void findDigitInFirstLine() {
-        for (int i = 0; i < 30; i++) {// Testing for 0 to 29 digits
-            System.out.println(i);
-            assertEquals("123456789101112131415161718192".charAt(i) - '0', Kata.nthDigitOfFirstLine(i));
-        }
-    }
-
-    @Test
-    public void find0thDigitISecondLine() {
-        assertThat(Kata.nthDigitOfSecondLine(0), is(1));
-    }
-
-    @Test
-    public void find1stDigitISecondLine() {
-        assertThat(Kata.nthDigitOfSecondLine(1), is(4));
-    }
-
-    @Test
-    public void find3rdDigitISecondLine() {
-        assertThat(Kata.nthDigitOfSecondLine(3), is(1));
-    }
-
-    @Test
-    public void find4thDigitISecondLine() {
-        assertThat(Kata.nthDigitOfSecondLine(4), is(6));
-    }
-
-    @Test
-    public void findDigitInSecondLine() {
-        for (int i = 0; i < 30; i++) {// Testing for 0 to 29 digits
-            System.out.println(i);
-            assertEquals("149162536496481100121144169196".charAt(i) - '0', Kata.nthDigitOfSecondLine(i));
-        }
-    }
-
-    @Test
-    public void findDigit0() {
-        assertThat(Kata.findDigit(0), is(2));
-    }
-
-    @Test
-    public void findDigit1() {
-        assertThat(Kata.findDigit(1), is(7));
-    }
-
-    @Test
-    public void findDigit5() {
-        assertThat(Kata.findDigit(5), is(9));
-    }
-
-    @Test
-    public void findDigit6() {
-        assertThat(Kata.findDigit(6), is(3));
-    }
-
-    @Test
-    public void findDigit17() {
-        assertThat(Kata.findDigit(17), is(1));
     }
 }
